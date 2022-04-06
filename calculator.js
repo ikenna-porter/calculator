28c934e9aa210644b23b756076c26f614f1a4baf
ikenna <ikenna.porter@gmail.com>
2022-04-06 15:10:44 +0200

    Work on operators' logic

--- calculator.js
+++ calculator.js
@@ -1,12 +1,60 @@
-const number = document.querySelector('.number');
+const numbers = document.querySelectorAll('.numbers');
+const operators = document.querySelectorAll(".operators");
 const input = document.querySelector('.recent-input');
+const operationsList = document.querySelector('.operations-list');
 
-let firstValue = 0;
-let secondValue = 0;
-number.addEventListener('click', e => {
-    let displayValue = e.target.innerText;
-    input.innerText = `${displayValue}`;
+
+let secondValue = '';
+let firstValue = '';
+let displayValue = input.innerText
+let operation;
+
+//Listens for "click" on numbers and prints number on screen
+numbers.forEach(function (num) {
+    num.addEventListener('click', e => {
+        firstValue += e.target.innerText;
+        displayValue = firstValue;
+    });
 });
+
+//Listens for click on each operator button if firstValue is true
+operators.forEach(operator => {
+    operator.addEventListener('click', e => {
+        if (firstValue) {
+            operationsList.innerText += `${firstValue} ${e.target.innerText} `;
+            firstValue = '';
+            operation = e.target.classList[1];
+        }
+    });
+});
+
+
+//Listens for click on one of the numebers if firstValue and operation are defined
+numbers.forEach(function (num) {
+    num.addEventListener('click', e => {
+        if (firstValue && operation) {
+            secondValue = e.target.innerText; //declare secondValue label
+            operationsList.innerText += ` ${secondValue} `; //adds secondValue to operationsList
+            //performs correct operation in accordance with the value from operation label
+            if (operation === 'add') {
+                console.log(firstValue);
+                console.log(secondValue);
+                console.log(operate(firstValue, secondValue, add));
+            } else if (operation === 'subtract') {
+                operate(firstValue, secondValue, subtract);
+            } else if (operation === 'multiply') {
+                operate(firstValue, secondValue, multiply);
+            } else if (operation === 'divide') {
+                operate(firstValue, secondValue, divide);
+            }
+        }
+    });
+});
+
+
+
+
+
 
 
 function add(a, b) {
