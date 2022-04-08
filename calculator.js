f4a1c84a648b85f5045e9ca1774976179353191d
ikenna <ikenna.porter@gmail.com>
2022-04-08 13:19:36 +0200

    Finish styling calculator 1) Add fonts 2) Background colors for buttons, screen, calculator and body updated 3) Update borders to buttons 4) Overflow set to scroll in calculator screen 5) Center calculator in body

--- calculator.js
+++ calculator.js
@@ -1,4 +1,5 @@
 const numbers = document.querySelectorAll('.numbers');
+const notNumbers = document.querySelectorAll('.not-numbers');
 const operators = document.querySelectorAll(".operators");
 const input = document.querySelector('.recent-input');
 const operationsList = document.querySelector('.operations-list');
@@ -14,6 +15,12 @@ let equalsRan = false;
 
 //Listens for "click" on numbers and prints number on screen
 numbers.forEach(function (num) {
+
+    //changes background color when hovered over element
+    num.addEventListener('mouseover', e => e.target.style.backgroundColor = "#563b3b");
+    //reverts back to normal background color when hovered out of element5c4141
+    num.addEventListener('mouseout', e => e.target.style.backgroundColor = "#5c4141");
+
     num.addEventListener('click', e => {
         operators.forEach(operator => operator.disabled = false);
         //If equals function was performed...
@@ -25,12 +32,22 @@ numbers.forEach(function (num) {
     });
 });
 
+notNumbers.forEach(function (notNum) {
+    notNum.addEventListener('mouseover', e => e.target.style.backgroundColor = "#492f1b");
+    notNum.addEventListener('mouseout', e => e.target.style.backgroundColor = "#593920");
+});
+
+equal.addEventListener('mouseover', e => e.target.style.backgroundColor = "#a15a23");
+equal.addEventListener('mouseout', e => e.target.style.backgroundColor = "#ae642b");
 
 //Listens for click on each operator button if firstValue is true
 //If no firstValue then operator buttons are ignored
 operators.forEach(operator => {
     operator.addEventListener('click', e => {
+        //turns off operator buttons once they've been pressed - prevents consecutive operator button pressing
         operators.forEach(operator => operator.disabled = true);
+
+
         updateOperationsList(e);
         updateOperation(e);
         //performs the operations and stores result as firstValue / resets secondValue to null
@@ -59,6 +76,10 @@ clear.addEventListener('click', e => {
     input.innerText = '';
 });
 
+del.addEventListener('click', e => {
+    deleteLastInput(e);
+})
+
 
 function logNumOnScreen(e) {
     //logs number on screen / allows first number to be 2+ digits
@@ -75,10 +96,13 @@ function updateOperationsList(e) {
     if (firstValue && !secondValue) { //if firstValue declared and secondValue not
         if (!operationsList.innerText) { //if operationsList is empty
             operationsList.innerText += `${firstValue} ${e.target.innerText}`; //then add firstVal + operation
-        } else { //if operationsList is not empty
+        } else if (equalsRan) {
+            operationsList.innerText = `${firstValue} ${e.target.innerText}`
+            equalsRan = false;
+        }else { //if operationsList is not empty
             operationsList.innerText += ` ${e.target.innerText}`; //then only add operator
         }
-    } else if (firstValue && secondValue) {
+    }else if (firstValue && secondValue) {
         operationsList.innerText += ` ${secondValue} ${e.target.innerText}`; //adds secondValue to operationsList
     }
 }
@@ -111,6 +135,13 @@ function performOperation() {
         //Resets secondValue to null
         secondValue = '';
     }
+}
+
+function deleteLastInput(e) {
+    //removes last input from operations list
+    operationsList.innerText = operationsList.innerText.slice(0, operationsList.innerText.length - 2);
+    secondValue = '';
+    // if (operation) operation = '';
 }
 
 function add(a, b) {
